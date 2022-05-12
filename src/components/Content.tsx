bf5e0f1586ed1f36bdc4e6e46403e9d0ab655f51
Bruno Vigel <brunorosav@gmail.com>
2022-05-12 17:58:06 -0300

    styles

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,7 @@
 import{useState, useEffect} from 'react';
 import {api} from '../services/api'
 import { MovieCard } from './MovieCard';
+import '../styles/content.scss';
 
 
 interface MovieProps {
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
 import { Button } from "./Button";
+import '../styles/sidebar.scss';
 
 
 
